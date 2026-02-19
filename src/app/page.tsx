0b4ab00bf6a939511c9fadd1be9daaaf7bdeb878
S.A. Robama <robamait@gmail.com>
2026-02-19 11:47:01 +0000

    falta el boton en la pantalla principal para registrar la salida

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { Truck, User } from 'lucide-react';
+import { Truck, User, LogOut } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
 import { DateTime } from '@/components/date-time';
@@ -15,7 +15,7 @@ export default function Home() {
         
         <DateTime />
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full max-w-4xl">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 w-full max-w-6xl">
           <Card className="transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl">
             <CardContent className="p-0">
               <Link href="/dashboard/personal/nuevo" className="flex flex-col items-center justify-center h-64 p-6 text-center rounded-lg">
@@ -31,6 +31,15 @@ export default function Home() {
                 <Truck className="w-20 h-20 mb-4 text-primary" />
                 <h2 className="text-2xl font-semibold text-card-foreground">Entrada Transportista</h2>
                 <p className="mt-2 text-muted-foreground">Registrar la entrada de camiones y vehículos de reparto.</p>
+              </Link>
+            </CardContent>
+          </Card>
+          <Card className="transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl">
+            <CardContent className="p-0">
+              <Link href="/dashboard/salida" className="flex flex-col items-center justify-center h-64 p-6 text-center rounded-lg">
+                <LogOut className="w-20 h-20 mb-4 text-primary" />
+                <h2 className="text-2xl font-semibold text-card-foreground">Registrar Salida</h2>
+                <p className="mt-2 text-muted-foreground">Registrar la salida de visitantes y transportistas.</p>
               </Link>
             </CardContent>
           </Card>
