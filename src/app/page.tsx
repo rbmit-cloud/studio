0ec849eac82d488a9e7644c7308c63e4d3ad7ca7
S.A. Robama <robamait@gmail.com>
2026-02-19 15:00:23 +0000

    el logo se ha hecho más pequeño, vuelve a cambiarle el tamaño

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,10 +10,10 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center p-8 bg-background">
       <div className="flex flex-col items-center gap-4">
         
-        <Image src="/robama-logo.jpg" alt="Logo de Robama S.A." width={800} height={200} className="w-[400px] h-auto mb-[-2rem]" />
+        <Image src="/robama-logo.jpg" alt="Logo de Robama S.A." width={800} height={200} className="w-[500px] h-auto mb-[-2rem]" />
         <DateTime />
 
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-8 w-full max-w-6xl mt-[-1rem]">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-8 w-full max-w-6xl">
           <Card className="transform transition-transform duration-300 hover:scale-105 hover:shadow-red-2xl">
             <CardContent className="p-0">
               <Link href="/dashboard/personal/nuevo" className="flex flex-col items-center justify-center h-64 p-6 text-center rounded-lg active:bg-destructive active:text-destructive-foreground">
