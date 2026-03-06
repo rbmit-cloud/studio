95860ce0775341852a28682fd73dd20fd4aa2b72
S.A. Robama <robamait@gmail.com>
2026-03-06 14:48:27 +0000

    Los días de la semana no están encima de los días, reajústalo

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -19,7 +19,7 @@ function Calendar({
   return (
     <DayPicker
       showOutsideDays={showOutsideDays}
-      className={cn("p-2", className)}
+      className={cn("p-1", className)}
       classNames={{
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
@@ -33,9 +33,9 @@ function Calendar({
         nav_button_previous: "",
         nav_button_next: "",
         table: "w-full border-collapse space-y-1",
-        head_row: "flex",
+        head_row: "flex w-full",
         head_cell:
-          "text-muted-foreground rounded-md w-7 font-normal text-[0.8rem]",
+          "text-muted-foreground text-center rounded-md w-7 font-normal text-[0.8rem]",
         row: "flex w-full mt-2",
         cell: "h-7 w-7 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
         day: cn(
