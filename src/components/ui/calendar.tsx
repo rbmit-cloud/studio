13b5ba56d4c7e9eae9edcd289e25f62c5b18cef2
S.A. Robama <robamait@gmail.com>
2026-03-06 14:23:38 +0000

    Ajusta el nombre de los días de la semana del calendario para que queden

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -53,6 +53,16 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
+      formatters={{
+        formatWeekdayName: (day, options) => {
+          const weekday = options?.locale?.localize?.day(day.getDay(), {
+            width: "short",
+          });
+          return weekday
+            ? weekday.charAt(0).toUpperCase() + weekday.slice(1)
+            : "";
+        },
+      }}
       components={{
         IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
         IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
