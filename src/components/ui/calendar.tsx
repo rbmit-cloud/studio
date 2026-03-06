2a4e7e40bd0b8fdda89cb3a480ca17a03868793c
S.A. Robama <robamait@gmail.com>
2026-03-06 14:27:05 +0000

    cambia el componente calendar por uno que tenga un formato distinto

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import * as React from "react"
+import { es } from "date-fns/locale"
 import { ChevronLeft, ChevronRight } from "lucide-react"
 import { DayPicker } from "react-day-picker"
 
@@ -36,15 +37,15 @@ function Calendar({
         head_cell:
           "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
         row: "flex w-full mt-2",
-        cell: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
+        cell: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
         day: cn(
           buttonVariants({ variant: "ghost" }),
-          "h-9 w-9 p-0 font-normal aria-selected:opacity-100"
+          "h-9 w-9 p-0 font-normal aria-selected:opacity-100 rounded-full"
         ),
         day_range_end: "day-range-end",
         day_selected:
           "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
-        day_today: "bg-accent text-accent-foreground",
+        day_today: "bg-accent text-accent-foreground rounded-full",
         day_outside:
           "day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground",
         day_disabled: "text-muted-foreground opacity-50",
@@ -54,14 +55,7 @@ function Calendar({
         ...classNames,
       }}
       formatters={{
-        formatWeekdayName: (day, options) => {
-          const weekday = options?.locale?.localize?.day(day.getDay(), {
-            width: "short",
-          });
-          return weekday
-            ? weekday.charAt(0).toUpperCase() + weekday.slice(1)
-            : "";
-        },
+        formatWeekdayName: (day) => es.localize?.day(day.getDay(), { width: 'short' }).substring(0,2) || ''
       }}
       components={{
         IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
