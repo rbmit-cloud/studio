0c329c23bc8384447a8d7e0391a4bdfd7fbc0e48
S.A. Robama <robamait@gmail.com>
2026-03-06 16:02:22 +0000

    Sigue saliendo mal, crea de nuevo el selector de calendario

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -31,12 +31,12 @@ function Calendar({
         ),
         nav_button_previous: "absolute left-1",
         nav_button_next: "absolute right-1",
-        table: "w-full border-collapse space-y-1",
-        head_row: "",
+        table: "w-full border-collapse",
+        head_row: "border-b",
         head_cell:
-          "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
-        row: "w-full mt-2",
-        cell: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
+          "text-muted-foreground p-2 text-center font-normal text-[0.8rem]",
+        row: "border-b",
+        cell: "text-center text-sm p-0 relative [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
         day: cn(
           buttonVariants({ variant: "ghost" }),
           "h-9 w-9 p-0 font-normal aria-selected:opacity-100"
