963a2e63baed1e728ca8b5e0ffeacc27392e904e
S.A. Robama <robamait@gmail.com>
2026-03-24 09:10:34 +0000

    hazlos más grandes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -209,24 +209,24 @@ export default function Home() {
         <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-8 w-full max-w-6xl">
           <Card className="transform transition-transform duration-300 hover:scale-105 hover:shadow-red-2xl cursor-pointer" onClick={() => handleEntryClick('/dashboard/personal/nuevo')}>
             <CardContent className="p-0">
-              <div className="flex flex-col items-center justify-center h-24 p-6 text-center rounded-lg active:bg-destructive active:text-destructive-foreground">
-                <User className="w-10 h-10 mb-2" />
+              <div className="flex flex-col items-center justify-center h-28 p-6 text-center rounded-lg active:bg-destructive active:text-destructive-foreground">
+                <User className="w-16 h-16 mb-2" />
                 <h2 className="text-xl font-semibold">{t('personalEntry')}</h2>
               </div>
             </CardContent>
           </Card>
           <Card className="transform transition-transform duration-300 hover:scale-105 hover:shadow-cyan-2xl cursor-pointer" onClick={() => handleEntryClick('/dashboard/transportista/nuevo')}>
             <CardContent className="p-0">
-              <div className="flex flex-col items-center justify-center h-24 p-6 text-center rounded-lg active:bg-info active:text-info-foreground">
-                <Truck className="w-10 h-10 mb-2 text-primary" />
+              <div className="flex flex-col items-center justify-center h-28 p-6 text-center rounded-lg active:bg-info active:text-info-foreground">
+                <Truck className="w-16 h-16 mb-2 text-primary" />
                 <h2 className="text-xl font-semibold text-card-foreground">{t('transporterEntry')}</h2>
               </div>
             </CardContent>
           </Card>
           <Card className="transform transition-transform duration-300 hover:scale-105 hover:shadow-yellow-2xl cursor-pointer" onClick={() => router.push('/dashboard/salida')}>
             <CardContent className="p-0">
-              <div className="flex flex-col items-center justify-center h-24 p-6 text-center rounded-lg active:bg-warning active:text-warning-foreground">
-                <LogOut className="w-10 h-10 mb-2 text-primary" />
+              <div className="flex flex-col items-center justify-center h-28 p-6 text-center rounded-lg active:bg-warning active:text-warning-foreground">
+                <LogOut className="w-16 h-16 mb-2 text-primary" />
                 <h2 className="text-xl font-semibold text-card-foreground">{t('registerExit')}</h2>
               </div>
             </CardContent>
