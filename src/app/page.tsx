ab13dab92ede3c79804276e5da915d07eeb153c2
S.A. Robama <robamait@gmail.com>
2026-03-24 12:59:37 +0000

    ajusta la pantalla principal para ejecutarse en vertical en una tablet d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -122,10 +122,10 @@ export default function Home() {
   };
 
   return (
-    <main className="flex flex-col items-center min-h-screen bg-background">
-      <div className="w-full max-w-sm px-4">
+    <main className="flex flex-col items-center min-h-screen bg-background p-4 justify-center">
+      <div className="w-full max-w-lg px-4">
         <div className="w-full relative bg-muted p-2 rounded-lg flex items-center justify-center mb-4">
-            <Image src="/robama-logo.jpg" alt="Logo de Robama S.A." width={250} height={66} className="h-auto" />
+            <Image src="/robama-logo.jpg" alt="Logo de Robama S.A." width={300} height={79} className="h-auto" />
             <div className="absolute right-4 top-4">
                 <DropdownMenu>
                 <DropdownMenuTrigger asChild>
@@ -204,42 +204,42 @@ export default function Home() {
         </AlertDialog>
 
 
-        <div className="w-full flex justify-center mb-4">
+        <div className="w-full flex justify-center mb-12">
           <DateTime />
         </div>
 
-        <div className="flex flex-col gap-4 w-full mt-4">
+        <div className="flex flex-col gap-6 w-full mt-4">
           <Card className="transform transition-transform duration-300 hover:scale-105 hover:shadow-red-2xl cursor-pointer" onClick={() => handleEntryClick('/dashboard/personal/nuevo')}>
             <CardContent className="p-0">
-              <div className="flex flex-col items-center justify-center gap-2 h-24 p-4 rounded-lg active:bg-destructive active:text-destructive-foreground">
-                <User className="w-8 h-8" />
-                <h2 className="text-lg font-semibold">{t('personalEntry')}</h2>
+              <div className="flex flex-col items-center justify-center gap-4 h-32 p-4 rounded-lg active:bg-destructive active:text-destructive-foreground">
+                <User className="w-10 h-10" />
+                <h2 className="text-xl font-semibold">{t('personalEntry')}</h2>
               </div>
             </CardContent>
           </Card>
           <Card className="transform transition-transform duration-300 hover:scale-105 hover:shadow-cyan-2xl cursor-pointer" onClick={() => handleEntryClick('/dashboard/transportista/nuevo')}>
             <CardContent className="p-0">
-              <div className="flex flex-col items-center justify-center gap-2 h-24 p-4 rounded-lg active:bg-info active:text-info-foreground">
-                <Truck className="w-8 h-8" />
-                <h2 className="text-lg font-semibold text-card-foreground">{t('transporterEntry')}</h2>
+              <div className="flex flex-col items-center justify-center gap-4 h-32 p-4 rounded-lg active:bg-info active:text-info-foreground">
+                <Truck className="w-10 h-10" />
+                <h2 className="text-xl font-semibold text-card-foreground">{t('transporterEntry')}</h2>
               </div>
             </CardContent>
           </Card>
           <Card className="transform transition-transform duration-300 hover:scale-105 hover:shadow-yellow-2xl cursor-pointer" onClick={() => router.push('/dashboard/salida')}>
             <CardContent className="p-0">
-              <div className="flex flex-col items-center justify-center gap-2 h-24 p-4 rounded-lg active:bg-warning active:text-warning-foreground">
-                <LogOut className="w-8 h-8" />
-                <h2 className="text-lg font-semibold text-card-foreground">{t('registerExit')}</h2>
+              <div className="flex flex-col items-center justify-center gap-4 h-32 p-4 rounded-lg active:bg-warning active:text-warning-foreground">
+                <LogOut className="w-10 h-10" />
+                <h2 className="text-xl font-semibold text-card-foreground">{t('registerExit')}</h2>
               </div>
             </CardContent>
           </Card>
         </div>
         
-        <div className="flex items-center justify-center gap-4 mt-6 w-full">
-            <Button variant="outline" onClick={() => handleOpenDialog('/dashboard/registros')}>
+        <div className="flex items-center justify-center gap-6 mt-8 w-full">
+            <Button variant="outline" size="lg" onClick={() => handleOpenDialog('/dashboard/registros')}>
               {t('viewVisitLog')}
             </Button>
-            <Button variant="outline" onClick={() => handleOpenDialog('/admin')}>
+            <Button variant="outline" size="lg" onClick={() => handleOpenDialog('/admin')}>
                 {t('administration')}
             </Button>
         </div>
